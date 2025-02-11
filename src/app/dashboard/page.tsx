07533a6c3561d2cb3b7a4e46d75a956ee1b868ad
thuong204 <thuongtc.22it@vku.udn.vn>
2025-02-11 23:21:59 +0700

    [Scrum-3] : Learn metadata group route

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+function BarChart() {
+  return (
+    <div>
+      <h2>Bar Chart</h2>
+    </div>
+  );
+}
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,13 +1,35 @@
-"use client";
-import { notFound, useParams } from "next/navigation";
-export default function ProductDetail() {
-  const params = useParams<{ productId: string }>();
-  if (parseInt(params.productId) > 1000) {
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+
+type Props = {
+  params: Promise<{
+    productId: string;
+  }>;
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const resolvedParams = await Promise.resolve(params);
+  const productId = resolvedParams.productId;
+
+  if (!productId) {
+    return {
+      title: "Product Not Found",
+      description: "The requested product does not exist.",
+    };
+  }
+
+  return {
+    title: `Product ${productId}`,
+    description: `This is the product page for ${productId}.`,
+  };
+}
+
+export default async function ProductDetail({ params }: Props) {
+  const productId = (await params).productId;
+
+  if (!productId || parseInt(productId) > 1000) {
     notFound();
   }
-  return (
-    <>
-      <div>Detail product {params.productId}</div>
-    </>
-  );
+
+  return <div>Detail product {productId}</div>;
 }
